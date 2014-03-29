7a54b56daa000436994f6e78517232c2b445f872
Matan Resnekov <matan99@gmail.com>
2014-03-29 19:51:08 +0300

    N queens BT

--- Ex_1/nqueen_backtracking.py
+++ Ex_1/nqueen_backtracking.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python
 
-BOARD_SIZE = 8
-
-
-def back(path):
+def back(path, n):
     if reject(path):
         return None
-    if is_solution(path):
-        print path
-    for brother in make_brothers(path):
-        if len(path) < BOARD_SIZE:
-            new_path = back(brother)
+    if is_solution(path, n):
+        return path
+    for brother in make_brothers(path, n):
+        if len(path) < n:
+            new_path = back(brother, n)
             if new_path:
                 return new_path
     return None
 
 
-def is_solution(path):
-    return len(path) >= BOARD_SIZE
+def is_solution(path, n):
+    return len(path) >= n
 
 
-def make_brothers(path):
+def make_brothers(path, n):
     path = path + [0]
-    while path[len(path) - 1] < BOARD_SIZE:
+    while path[len(path) - 1] < n:
         yield path
         path[len(path) - 1] += 1
 
@@ -31,10 +28,19 @@ def reject(path):
     t = False
     for i in range(len(path)):
         for j in range(i + 1, len(path)):
+            # checks if any two queens are on the same diagonal or same row
             if abs((path[j] - path[i]) * 1.0 / (j - i)) == 1 or path[j] == path[i]:
                 return True
 
     return t
 
 
-back([])
+# MAIN
+import time
+start_time = time.time()
+
+for i in range(4, 11, 2):
+    print 'N =', i
+    print 'solution: ', back([], i)
+    print time.time() - start_time, "seconds"
+    print '----'
